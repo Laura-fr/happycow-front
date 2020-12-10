a8d9c22a7ce5dbfe4e016c9b2255e35093395fdb
Laura-fr <laurafraysse84@gmail.com>
2020-12-10 21:01:52 +0100

    bug fixed

--- src/components/Modale.js
+++ src/components/Modale.js
@@ -1,35 +1,33 @@
 import React from "react";
 import modalepic from "../assets/login-social.jpg";
 
-const Modale = ({ revele, cache }) =>
-  revele ? (
+const Modale = ({ revele, cache }) => {
+  return revele ? (
     <div>
       <div className="overlay"></div>
       <div className="wrapper">
-        <div className="modale">
-          <div>
-            <img src={modalepic} alt="modalepic" />
-          </div>
-          <div className="alllogin">
-            <button className="close" onClick={cache}>
-              <span>&times;</span>
-            </button>
-            <div className="login">
-              <h2>Login to Your Account</h2>
-              <p>Username or Email</p>
-              <input />
-              <p>Password</p>
-              <input type="password" />
-              <input className="submit" type="button" value="Login" />
+        {/* <div className="modale"> */}
+        <div>
+          <img src={modalepic} alt="modalepic" />
+        </div>
+        <div className="alllogin">
+          <button className="close" onClick={cache}>
+            <span>&times;</span>
+          </button>
+          <div className="login">
+            <h2>Login to Your Account</h2>
+            <p>Username or Email</p>
+            <input />
+            <p>Password</p>
+            <input type="password" />
+            <input className="submit" type="button" value="Login" />
 
-              <input className="submit" type="button" value="Register" />
-            </div>
+            <input className="submit" type="button" value="Register" />
           </div>
         </div>
+        {/* </div> */}
       </div>
     </div>
   ) : null;
-{
-}
-
+};
 export default Modale;
