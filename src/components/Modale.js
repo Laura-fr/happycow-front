a9607fc45131f3a375d46d59cc08dafa285bc094
Laura-fr <laurafraysse84@gmail.com>
2020-12-10 22:14:33 +0100

    css

--- src/components/Modale.js
+++ src/components/Modale.js
@@ -6,26 +6,26 @@ const Modale = ({ revele, cache }) => {
     <div>
       <div className="overlay"></div>
       <div className="wrapper">
-        {/* <div className="modale"> */}
-        <div>
-          <img src={modalepic} alt="modalepic" />
-        </div>
-        <div className="alllogin">
-          <button className="close" onClick={cache}>
-            <span>&times;</span>
-          </button>
-          <div className="login">
-            <h2>Login to Your Account</h2>
-            <p>Username or Email</p>
-            <input />
-            <p>Password</p>
-            <input type="password" />
-            <input className="submit" type="button" value="Login" />
+        <div className="modale">
+          <div>
+            <img src={modalepic} alt="modalepic" />
+          </div>
+          <div className="alllogin">
+            <button className="close" onClick={cache}>
+              <span>&times;</span>
+            </button>
+            <div className="login">
+              <h2>Login to Your Account</h2>
+              <p>Username or Email</p>
+              <input />
+              <p>Password</p>
+              <input type="password" />
+              <input className="submit" type="button" value="Login" />
 
-            <input className="submit" type="button" value="Register" />
+              <input className="submit" type="button" value="Register" />
+            </div>
           </div>
         </div>
-        {/* </div> */}
       </div>
     </div>
   ) : null;
